5d66483b4eaab0c963fa5dc7105065a14121b7d3
rdev0002git <67629853+rdev0002git@users.noreply.github.com>
2023-08-31 00:53:14 +0300

    feat, docs: изменена работа функции, добавлены описания и комментарии

--- src/views.py
+++ src/views.py
@@ -11,8 +11,14 @@ from src.tools import gen_random_tree, hdf5_read_recursive, hdf5_write_recursive
 
 
 class IntDelegate(QtWidgets.QItemDelegate):
+    '''Класс реализующий методы для создания, настройки и валидации редактора элемента дерева TreeView.'''
 
     def createEditor(self, parent: QtWidgets.QWidget, option: QtWidgets.QStyleOptionViewItem, index: QtCore.QModelIndex):
+        '''
+        Создает редактор QLineEdit с ограничением на ввод определенных данных,
+        допускаются только int значения
+        '''
+
         editor = QtWidgets.QLineEdit(parent)
         editor.setValidator(QtGui.QIntValidator())
         return editor
@@ -23,12 +29,13 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
     def __init__(self, tree_view_model: TreeViewModel):
         super().__init__()
 
-        # Инициализация UI
+        # Построение UI элемента
         self.setupUi(self)
         self.graph_widget = pyqtgraph.PlotWidget()
         self.graph_plot = self.graph_widget.plot([], [])
         self.graphLayout.addWidget(self.graph_widget)
 
+        # Установка делегата для элементов TreeView
         self.treeView.setItemDelegate(IntDelegate())
         self.addTreeItemEdit.setValidator(QtGui.QIntValidator())
 
@@ -36,7 +43,7 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
         self.model = tree_view_model
         self.treeView.setModel(self.model)
 
-        # Подключения к сигналам
+        # Подключения методов к сигналам
         self.addTreeItemButton.clicked.connect(self.add_tree_item)
         self.deleteTreeItemButton.clicked.connect(self.delete_tree_item)
         self.loadDataButton.clicked.connect(self.load_data)
@@ -47,9 +54,9 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
 
     # TREEVIEW
     def add_tree_item(self):
-        '''Функция добавляющая элемент в TreeView'''
+        '''Добавить элемент в TreeView'''
 
-        # Получение указанного в LineEdit значения
+        # Получение введенного пользователем значения из элемента GUI
         value = self.addTreeItemEdit.text()
 
         # Валидация введенного значения
@@ -66,13 +73,14 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
         self.update_graph()
 
     def delete_tree_item(self):
-        '''Функция удаляющая выделенные элементы в TreeView'''
+        '''Удалить выделенные элементы из TreeView'''
 
         # Получение индексов выделенных элементов
         indexes = self.treeView.selectedIndexes()
 
         # Сортировка индексов от большего к меньшему, чтобы избежать проблемы смещения индексов при удалении
         indexes = sorted(indexes, key=lambda i: i.row(), reverse=True)
+        indexes = sorted(indexes, key=lambda i: i.parent().row(), reverse=True)
 
         # Последовательное удаление элементов
         for index in indexes:
@@ -82,8 +90,9 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
 
     # SIDEBAR
     def load_data(self):
-        '''Функция загрузки данных в модель TreeView'''
+        '''Загрузить данные в TreeView'''
         
+        # Вызов диалогового окна для получения пути к файлу и требуемые тип файла из которого будут загружаться данные
         file_path, file_path_filters = QtWidgets.QFileDialog().getOpenFileName(self, 'Открыть файл', '.', '*.json;;*.hdf5')
         if file_path == '': return
 
@@ -98,8 +107,9 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
         self.update_graph()
 
     def save_data(self):
-        '''Функция сохранения данных из модели TreeView'''
+        '''Сохранить данные из TreeView'''
 
+        # Вызов диалогового окна для получения пути к файлу и требуемые тип файла в котором нужно сохранить данные
         file_path, file_path_filters = QtWidgets.QFileDialog().getSaveFileName(self, 'Сохранить файл', '.', '*.json;;*.hdf5')
         if file_path == '': return
 
@@ -113,46 +123,57 @@ class MainView(Ui_mainWidget, QtWidgets.QWidget):
                 hdf5_write_recursive(file, data)
 
     def load_randomize_data(self):
-        '''Функция заполнения TreeView рандомными данными'''
-        
+        '''Заполнить TreeView рандомными данными'''
+
         data = gen_random_tree(3, 10, -33, 33, 4)
         self.model.load_data(data)
 
         self.update_graph()
 
     def update_graph(self):
+        '''Обновить график'''
         
-        data = self.model.get_data()
-
         def prepare_arr_for_graph(data: list, level: int = 1):
+            '''
+            Извлечь данные, из указанного иерархического списка,
+            в виде массива: level - уровень вложенности в дереве, value- значение
+            '''
 
             arr: list[tuple[int | int]] = []
 
             for data_item in data:
 
                 if isinstance(data_item, list):
-                    arr += prepare_arr_for_graph(data_item, level + 1)
+                    # Рекурсивное извлечение данных из вложенного списка
+                    values = prepare_arr_for_graph(data_item, level + 1)
+                    arr.append((level, sum([value[1] for value in values])))
+                    arr += values
                 else:
                     arr.append((level, data_item))
 
             return arr
 
+        # Получение данных из модели TreeView
+        data = self.model.get_data()
+
+        # Очистка данных графика
         self.graph_widget.plotItem.clear()
 
+        # Если есть данные на основе которых можно построить график
         if len(data) > 0:
-
+            
+            # Преобразование данных в двумерный массив
             data_for_graph = prepare_arr_for_graph(data)
-
-            # Преобразование в numpy массив
             data_array = numpy.array(data_for_graph)
 
             # Извлечение уникальных уровней
             levels = numpy.unique(data_array[:, 0])
 
-            # Вычисление сумм для каждого уровня
-            level_sums = [numpy.sum(data_array[data_array[:, 0] == level, 1]) for level in levels]
+            # Вычисление сумм лепестков для каждого уровня
+            level_sums = [numpy.average(data_array[data_array[:, 0] == level, 1]) for level in levels]
 
             # Создание нового двухмерного массива вида (level, level sum)
             result_array = numpy.column_stack((levels, level_sums))
 
+            # Построение графика
             self.graph_widget.plot(result_array[:, 0], result_array[:, 1])
